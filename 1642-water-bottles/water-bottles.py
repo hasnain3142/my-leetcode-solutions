4b1e68789973ec29e98458de10fe55a3552dc8d0
Hasnain Ali <beinghasnain16@gmail.com>
2024-07-07 18:50:33 +0500

    Time: 35 ms (52.78%) | Memory: 16.6 MB (32.78%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -4,16 +4,9 @@ class Solution:
         drinks = numBottles
         numBottles = 0
         while empty >= numExchange:
-            print("numBottles", numBottles)
-            print("empty", empty)
-            print("drinks", drinks)
             numBottles = empty // numExchange
             empty = empty % numExchange
             drinks += numBottles
             empty += numBottles
-            print("numBottles", numBottles)
-            print("empty", empty)
-            print("drinks", drinks)
-            print("loope nd")
             
         return drinks
