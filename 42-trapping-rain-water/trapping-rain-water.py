a0b4ad98ea0e0e18af4e3c713bb094fd383d383e
Hasnain Ali <beinghasnain16@gmail.com>
2024-07-18 15:57:18 +0500

    Time: 96 ms (77.48%) | Memory: 18.3 MB (81.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,15 +1,30 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        max_left = [0]
-        max_right = [0]
+        if not height:
+            return 0
+        res = 0
+        l,r = 0, len(height)-1
+        leftMax, rightMax = height[l], height[r]
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
+#         max_left = [0]
+#         max_right = [0]
         
-        for l in height:
-            max_left.append(max(l,max_left[-1]))
-        for r in height[::-1]:
-            max_right.append(max(r, max_right[-1]))
-        traps = 0
-        for i in range(len(height)):
-            water = min(max_left[i], max_right[-i-1]) - height[i]
-            if water > 0:
-                traps += water
-        return traps
+#         for l in height:
+#             max_left.append(max(l,max_left[-1]))
+#         for r in height[::-1]:
+#             max_right.append(max(r, max_right[-1]))
+#         traps = 0
+#         for i in range(len(height)):
+#             water = min(max_left[i], max_right[-i-1]) - height[i]
+#             if water > 0:
+#                 traps += water
+#         return traps
